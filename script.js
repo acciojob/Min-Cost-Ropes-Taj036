ac76263122954084b2b3928c57f3f2fd63bf8762
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-10 01:31:33 +0000

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let sorted = arr.slice().sort(a , b => a-b);
+	let totalcost = 0 ;
+	while(sorted.length > 1){
+		let rope1 = sorted.shift();
+		let rope2 = sorted.shift();
+		let combine = rope1 + rope2 ;
+		totalcost += combine ;
+		sorted.push(totalcost) ;
+		sorted.sort(a , b => a-b);
+	}
+	return totalcost ;
   
 }
 
